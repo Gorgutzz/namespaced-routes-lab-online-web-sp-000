57ee5cc0544e02d5ed16bfb2ea77ceea06df68ed
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-08-03 23:46:08 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -12,7 +12,13 @@ class ArtistsController < ApplicationController
   end
 
   def new
+    if p = Preference.first
+  if p.allow_create_artists
     @artist = Artist.new
+  else
+    redirect_to artists_path
+  end
+end
   end
 
   def create
